793b7b3b31f0bb756dd9b1e62e62e6e2e7f9a643
abdulrfarag <ar060290@gmail.com>
2023-01-30 16:20:28 -0600

    setup init commit

--- index.js
+++ index.js
@@ -1,48 +1,18 @@
 // TODO: Include packages needed for this application
-const fs = require('fs');
-const path = require('path');
-const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown');
+
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        name: 'github',
-        message: 'What is your GitHub username?',
-    },
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?'
-    },
-     {
-        type: 'input',
-        name: 'description',
-        message: 'What is your description'
-     },
-];
+
 
 // TODO: Create a function to write README file
-function writeToFile(data) {
-    return fs.writeFileSync(path.join(process.cwd(), 'New_Readme.md'), data)
-}
+
 
 // TODO: Create a function to initialize app
-function init() {
-    // ask the questions
-    inquirer.prompt(questions).then((answers) => {
-
-        console.log(answers)
-        writeToFile(generateMarkdown(answers));
-
-    })
 
     // format the answers into a markdown string
 
     // pass that string to the writeToFile function
 
 
-}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,31 +1,16 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(answers) {
 
-  return `
-   # ${answers.title}
-   ${answers.description}
-   ## This is a subtitle
-   This is a paragraph below the subtitle
-
-   - List item 1
-   - List Item 2
-   - List Item 3
-
-   Visit my GitHub page: ${answers.github}
-
-  `
-}
 
 module.exports = generateMarkdown;
